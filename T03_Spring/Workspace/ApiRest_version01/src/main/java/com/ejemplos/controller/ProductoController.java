adbf96c8c7edbd8c5b44b8e2dbd2e6f1156d1506
ManuelVR03 <manuel.verdejo.ramirez.alu@iesjulioverne.es>
2025-01-20 10:24:36 +0100

    Agregar métodos para gestionar productos en ProductoController: obtener, crear, editar y eliminar.

--- T03_Spring/Workspace/ApiRest_version01/src/main/java/com/ejemplos/controller/ProductoController.java
+++ T03_Spring/Workspace/ApiRest_version01/src/main/java/com/ejemplos/controller/ProductoController.java
@@ -2,7 +2,12 @@ package com.ejemplos.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ejemplos.modelo.Producto;
@@ -21,5 +26,60 @@ public class ProductoController {
 		return productoRepositorio.findAll();
 	}
 	
+	/* @PathVariable: permite inyectar un fragmento de la URL
+	 * en una variable, es decir, pasa el valor del id
+	 * de la URL al método como parámetro donde esté @PathVariable
+	 */
+	@GetMapping("/producto/{id}")
+	public Producto obtenerUno(@PathVariable Long id) {
+		return productoRepositorio.findById(id).orElse(null);
+	}
 	
+	/**
+	 * Insertar un nuevo producto
+	 */
+	
+	/*
+	 * @RequestBody Permite inyectar el cuerpo de la petición de un objeto,
+	 * guardo en nuevo lo que venga en el body de la petición
+	 */
+	@PostMapping("/producto")
+	public Producto nuevoProducto(@RequestBody Producto nuevo) {
+		return productoRepositorio.save(nuevo);
+	}
+	
+	/**
+	 * Actualiza un producto
+	 */
+	
+	@PutMapping("/producto/{id}")
+	public Producto editarProducto(@RequestBody Producto editar, @PathVariable Long id) {
+		if(productoRepositorio.existsById(id)) {
+			editar.setId(id);
+			return productoRepositorio.save(editar);
+		}else
+			return null;
+	}
+	
+	/**
+	 * Eliminar un producto
+	 */
+	
+	/*
+	@DeleteMapping("/producto/{id}")
+	public void eliminarProducto(@PathVariable Long id) {
+		if(productoRepositorio.existsById(id))
+			productoRepositorio.deleteById(id);
+	}
+	*/
+	
+	@DeleteMapping("/producto/{id}")
+	public Producto borrarProducto(@PathVariable Long id) {
+		if(productoRepositorio.existsById(id)) {
+			Producto result = productoRepositorio.findById(id).get();
+			productoRepositorio.deleteById(id);
+			return result;
+		}else
+			return null;
+	}
 }
